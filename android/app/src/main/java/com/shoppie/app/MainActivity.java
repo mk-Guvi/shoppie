59f57074e86b44ebeb3783be993256267a53c7d4
Mohammed Musharaf <mohammedm@zuper.co>
2025-05-04 20:41:04 +0530

    [FEAT] added capacitor js

---
+++ android/app/src/main/java/com/shoppie/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.shoppie.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shoppie.app',
+  appName: 'shoppie',
+  webDir: 'dist'
+};
+
+export default config;
