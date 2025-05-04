aa6925199907f9a8c37f50d30b5597174f7ca875
Mohammed Musharaf <mohammedm@zuper.co>
2025-05-04 20:59:38 +0530

    [FEAT] added legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  plugins: [react()],
+  plugins: [react(), legacy()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
